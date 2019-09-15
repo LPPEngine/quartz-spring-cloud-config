3d251cbc12c2995b24b5bf177e3e03fc1e9349a0
Kenny <1229672848@qq.com>
2019-09-15 10:16:48 +0800

    update enum

--- src/main/java/com/example/quartz/enums/JobTypeEnum.java
+++ src/main/java/com/example/quartz/enums/JobTypeEnum.java
@@ -4,26 +4,32 @@ import com.example.quartz.jobs.entity.FeedJob;
 import com.example.quartz.jobs.entity.PushHotelJob;
 
 public enum  JobTypeEnum {
-
+    /**
+     * Push Hotel price job
+     */
     PushHotelJob_TYPE_ENUM("PushHotelJob", PushHotelJob.class),
+    /**
+     * generate feed file job
+     */
     FeedJob_TYPE_ENUM("FeedJob", FeedJob.class);
+
     private String jobType;
     private Class jobClass;
+
+    public Class getJobClass() {
+        return jobClass;
+    }
+
+    public void setJobClass(Class jobClass) {
+        this.jobClass = jobClass;
+    }
 
     public void setJobType(String jobType) {
         this.jobType = jobType;
     }
 
-    public void settClass(Class tClass) {
-        this.jobClass = tClass;
-    }
-
     public String getJobType() {
         return jobType;
-    }
-
-    public Class gettClass() {
-        return jobClass;
     }
 
     JobTypeEnum(String jobType, Class jobClass) {
