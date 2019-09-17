5080198b999253b038e1abaefc9fc1eb7325d45e
ksong <1229672848@qq.com>
2019-09-17 17:55:31 +0800

    add "@PersistJobDataAfterExecution @DisallowConcurrentExecution" for feed job

--- src/main/java/com/example/quartz/jobs/entity/FeedJob.java
+++ src/main/java/com/example/quartz/jobs/entity/FeedJob.java
@@ -1,11 +1,15 @@
 package com.example.quartz.jobs.entity;
 
 import com.example.quartz.tasks.template.BaseTasksTemplate;
+import org.quartz.DisallowConcurrentExecution;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
+import org.quartz.PersistJobDataAfterExecution;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.quartz.QuartzJobBean;
 
+@PersistJobDataAfterExecution
+@DisallowConcurrentExecution
 public class FeedJob extends QuartzJobBean {
 
     @Autowired
